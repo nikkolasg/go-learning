4ead178688b31cf53c364a4243a5a1346698875e
nikko <nikko@localhost.localdomain>
2015-06-16 08:00:59 +0000

    receiver weird

---
+++ receiver.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type Walker interface {
+    walk(nb int) bool
+}
+
+type Human struct {
+    name string
+}
+
+type Robot struct {
+    model string
+}
+
+func (h *Human) walk(nb int) bool {
+    fmt.Println(h.name," has walked ", nb, " meters !")
+    return true
+}
+func (r *Robot) walk(nb int) bool {
+    fmt.Println(r.model," has walked ",nb," meters !")
+    return true
+}
+
+func move(w Walker) {
+    if w.walk(10) {
+        fmt.Println("It has walked !!")
+    } else {
+        fmt.Println("It did not walked :/ ")
+    }
+}
+func describe(h Human) {
+    fmt.Println("Human named : ", h.name)
+}
+func main () {
+    h1 := Human{name: "Robert" }
+    r1 := Robot{model: "R2D2" }
+    move(&h1)
+    move(&r1)
+    describe(&h1)
+}
