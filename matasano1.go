0b9c1761cbc118afc9521d4a19da74cdc8edf435
nicolas gailly <gailly@ned-team.com>
2015-06-16 17:37:14 +0200

    xored input

--- matasano1.go
+++ matasano1.go
@@ -1,5 +1,7 @@
 package main
 import "fmt"
+import "errors"
+import "encoding/hex"
 
 const b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 
@@ -21,7 +23,7 @@ func hex2base64(src []byte) []byte {
         b3 := (group24 >> 6) & 0x3F
         b4 := (group24 >> 0) & 0x3F  
         
-        fmt.Printf("[h1,h2,h3] = [%d,%d,%d] || [b1,b2,b3,b4] = [%d,%d,%d,%d]\n",h1,h2,h3,b1,b2,b3,b4) 
+        //fmt.Printf("[h1,h2,h3] = [%d,%d,%d] || [b1,b2,b3,b4] = [%d,%d,%d,%d]\n",h1,h2,h3,b1,b2,b3,b4) 
         
         dst = append(dst,byte(b64[b1]))
         dst = append(dst,byte(b64[b2]))
@@ -34,9 +36,28 @@ func hex2base64(src []byte) []byte {
     return dst
 }
 
+func fixedXOR(a,b []byte) ([]byte,error) {
+    if len(a) != len(b) {
+        return nil,errors.New("Buffer are not equal sized!")
+    }
+    c := make([]byte,len(a))
+    for i:= 0; i < len(a); i++ {
+        c[i] = a[i] ^ b[i]
+    }
+    return c,nil
+}
+
+
 func main() {
     input := []byte("Man is distinguished")
     out := hex2base64(input)
-    fmt.Printf("input : %s ==> %s\n",string(input),string(out))
+    fmt.Printf("1. input : %s ==> %s\n",string(input),string(out))
+    input1 := "1c0111001f010100061a024b53535009181c"
+    input1h,_ := hex.DecodeString(input1)
+    input2 := "686974207468652062756c6c277320657965"
+    input2h,_ := hex.DecodeString(input2)
+    xored, _ := fixedXOR(input1h,input2h)
+    xord := hex.EncodeToString(xored)
+    fmt.Printf("2. output : %s\n",xord)
 }
 
